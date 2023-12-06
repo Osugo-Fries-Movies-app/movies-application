9ccb9abcba9731209fcb0a0245c26bbeb23aef5e
T-Asia Fries <tasia.s.fries@gmail.com>
2023-12-06 11:19:45 -0600

    added function and modals for editing and adding movies

--- keys.js
+++
@@ -1,2 +0,0 @@
-const POSTER_API_KEY =  "http://img.omdbapi.com/?i=tt3896198&h=600&apikey=4412c32c";
-const OMDB_API_KEY =  "http://www.omdbapi.com/?i=tt3896198&apikey=4412c32c";
--- movies-api.js
+++ movies-api.js
@@ -12,28 +12,50 @@ export const getAllMovies = async () => {
     }
 }
 
-// ADDING A MOVIE
-
-// export const addMovie = async (movies) => {
-//     try {
-//         const url = `http://localhost:3000/movies`;
-//         const options = {
-//             method: 'POST',
-//             body: JSON.stringify(movies)
-//         };
-//         const response = await fetch (url, options);
-//         const newMovie = await response.json();
-//         return newMovie;
-//     } catch (error) {
-//         console.log(error);
-//     }
+// function showMovies() {
+//     fetch('http://localhost:3000/movies')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//         })
 // }
+// showMovies()
 
-function showMovies() {
-    fetch('http://localhost:3000/movies')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
+// FUNCTION CREATING MOVIE CARDS
+function movieCards(movie) {
+    return `
+        <div class="col-md-3">
+            <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="${POSTER_API_KEY}" alt="${movie.title}">
+                <div class="card-body">
+                    <h2>${movie.title}</h2>
+                    <p>Rating: ${movie.rating}</p>
+                    <p>Showtimes: 2:00PM 3:15PM 5:30PM 6:45PM 7:20pm 8:15PM</p>
+                    <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
+                </div>
+            </div>
+        </div>
+    `;
 }
-showMovies()
+
+// FUNCTION THAT DISPLAYS LOADING IMG THEN POPULATES MOVIES
+async function showMovies() {
+    try {
+        const moviesContainer = document.getElementById('movies');
+        const loadingImg = document.getElementById('img');
+        loadingImg.style.display = 'block';
+        const movies = await getAllMovies();
+        moviesContainer.innerHTML = '';
+        movies.forEach(movie => {
+            moviesContainer.innerHTML += movieCards(movie);
+        });
+        loadingImg.style.display = 'none';
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', showMovies);
+
+// EDITING A MOVIE
+
